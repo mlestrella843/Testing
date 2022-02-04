4430133aa1dd76173e9174b24f7dd32bfd10f803
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-02-04 16:50:05 +0000

    sistema que dice el precio que deberas pagar en base a tu edad y tus ingresos.

--- .ccls-cache/@home@runner@Testing/main.cpp
+++ .ccls-cache/@home@runner@Testing/main.cpp
@@ -2,44 +2,39 @@
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
-	int a, b, c;  // 
-	
-  cout<< "Introduce 3 integers to calcula which is largest \n";
-  cin>>a;
-  cin>>b;
-  cin>>c;
-  
+	int age;
+  double income;
+  string city_employment; 
 
-if (a>b && a>c){
-    cout<<"a is the integer greater\n" <<a;
-}
-else if (b>a && b>c){
-    cout<<"b is the integer greater\n" <<b;
-}
-else if(c>a && c>b){
-  cout<<"c is the integer greater\n" <<c;
-}
-  
- return 0;
-}
+  cout<<"This is the public transportation system.\nPlease answers the question to indicated what you have to pay.\n\n";
+  cout<<"Let's start!\n\n";
+  cout<<"Please enter your Age:\n";cin>>age;
+  cout<<"Please enter your Income:\n";cin>>income;
+  cout<< "Are you a City Employee? Please answer Yes or No: \n";
+  cin>>city_employment;
 
+  if(city_employment=="yes"){
+    cout<< "Please pay only $50\n";
+  }
+    else if( age >=0  && age <=5)
+      { cout<< "Kids under 5 years old don't pay\n";}
+    else if( age >= 6 && age <=17) 
+      { cout<< "Please pay $80 dollars\n"; }
+    else if( age >= 18 && income <=15000)
+      { cout<< "Please pay $30 dollars\n";}       
+    else if(age >= 65 && income <=30000) 
+      {  cout<< "Please pay $70 dollars\n";}
+        //    }   ) 
+        else { 
+        cout<< "You are part of General Population, Please pay only $100\n\n"; }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return 0;
+    
+    
+    }
 
 
 
