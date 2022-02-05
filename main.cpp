9f53533a38c808aed67039c40a4d8995dbc3e8a5
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-02-05 17:35:39 +0000

    show what lowercase letter the user entered

--- main.cpp
+++ main.cpp
@@ -1,31 +1,32 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
 
-int n, number_day;
-cout<<"Introduce a number and you will know which day of the week it is\n";
-cin>>number_day;
+int i,j=0;
+char letter;
+char array_vowel[5]={'a','e','i','o','u'};
+char array_conso[22]={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z'};
 
-if(number_day>0 && number_day<=7)
- {   
+cout<<"Favor introducir la letra minuscula: \n";
+cin>>letter;
 
-  switch(number_day){
-  case 1: cout<<"Sunday\n"; break;
-  case 2: cout<<"Monday\n";break;
-  case 3: cout<<"Tuesday\n";break;
-  case 4: cout<<"Wednesday\n";break;
-  case 5: cout<<"Thursday\n";break;
-  case 6: cout<<"Friday\n";break;
-  case 7: cout<<"Saturday\n";break;
+for (i=0; i<=5; i++){
+  if(array_vowel[i]== letter){
+    cout<<"la letra introducida es la vocal: " <<array_vowel[i];break;
   }
-
- }
-
-else 
-cout<<"The week only have 7 days\n";
-
-return 0;
-
 }
+
+for (j=0; j<=22; j++){
+  if(array_conso[j]== letter){
+    cout<<"la letra introducida es la consonante: " <<array_conso[j];break;
+  }
+       else if('y'== letter){
+       cout<<"la letra introducida es la 'y' que puede ser vista como vocal o como consonante: "; break;
+       }     
+       //cout<<"la letra introducida es invalida: \n"; break;
+    }
+   return 0;
+}
+
+
