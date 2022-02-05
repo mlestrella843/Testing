cb5916d0573c30306ff0fa7522f80966d45ed549
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-02-05 01:44:37 +0000

    Sistema que dice el dia que es.

---
+++ .ccls-cache/@home@runner@Testing/lowercase.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int i,j,num;
+char arreglo[]= "1,2,3,4,5";
+
+for (i =0; i<5; i++){
+
+ cout<<"imprime lo que hay en el arreglo\n" 
+ <<arreglo[i]; 
+
+
+}
+
+
+
+  return 0;
+}
